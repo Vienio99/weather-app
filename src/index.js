a575caec986cdd282fd28a01aaf2d265066b47a2
Vienio99 <kyve96@gmail.com>
2021-06-03 14:27:50 +0200

    displaying parsed data

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
+
 (function () {
-  const currentWeather = document.querySelector('.current-weather')
+  const currentCity = document.getElementById('city')
+  const currentTemperature = document.getElementById('current-temperature')
+  const currentHumidity = document.getElementById('current-humidity')
+
   const searchBtn = document.getElementById('search-btn')
   const searchForm = document.forms[0]
 
   searchBtn.addEventListener('click', (e) => {
     e.preventDefault()
-    const city = searchForm.search.value
-    getCurrentWeather(city)
+    const citySearch = searchForm.search.value
+    getCurrentWeather(citySearch)
+  })
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      const citySearch = searchForm.search.value
+      getCurrentWeather(citySearch)
+    }
   })
 
   async function getCurrentWeather (city) {
     try {
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=97015c07e527f4019713d1dd80c9f0e8`)
+      const response = await window.fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=97015c07e527f4019713d1dd80c9f0e8`)
       response.json().then(function (response) {
         if (!response.weather) {
           throw Error('No records found')
         } else {
-          console.log(response.weather[0].description)
-          currentWeather.textContent = response.weather[0].description
+          console.log(response)
+          currentCity.textContent = response.name
+          currentTemperature.textContent = 'Temperature: ' + response.main.temp + '°C'
+          currentHumidity.textContent = 'Humidity: ' + response.main.humidity + '%'
         }
       })
     } catch (err) {
